657a5127dab2ee98c493b71876e9407400492546
Mr Cozzens <acozzens@hotmail.com>
2019-09-13 21:42:20 -0700

    initial checkin empty repo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pi3-win10-iot-SimpleClock
 Really simple clock. Primarily designed for Raspberry Pi 3 with Windows 10 IOT installed.
+
+1. Add the text block to the MainPage.xaml designed
+```XML
+    <Grid>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto"/>
+        </Grid.ColumnDefinitions>
+        <TextBlock x:Name="txtTime" Grid.Row = "0" Grid.Column = "0"  Text="Initializing Time"
+           IsTextSelectionEnabled="False" TextWrapping="Wrap" TextAlignment="Center" HorizontalAlignment="Center" VerticalAlignment="Center" FontSize="140" />
+    </Grid>
+```
+
+2. Add a ticker for the  clock engine.  MainPage.xaml.cs
---
+++ pi3-win10-iot-SimpleClock/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace pi3_win10_iot_SimpleClock
+{
+    /// <summary>
+    /// Primary Clock Page Guts
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
